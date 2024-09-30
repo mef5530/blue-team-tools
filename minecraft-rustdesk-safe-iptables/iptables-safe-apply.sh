83f48642d4eabe7d77edba0796c6eb0db0ca35d8
mef5530 <70184066+mef5530@users.noreply.github.com>
2024-09-30 19:44:24 -0400

    fix wrong times

--- minecraft-rustdesk-safe-iptables/iptables-safe-apply.sh
+++ minecraft-rustdesk-safe-iptables/iptables-safe-apply.sh
@@ -5,11 +5,11 @@ iptables-restore < lockdown-iptables.rules
 (sleep 300 && iptables-restore < backup-iptables.rules) &
 
 echo "type confirm"
-read -t 60 confirm
+read -t 300 confirm
 
 if [ "$confirm" ]; then
         echo "W!"
-        pkill -f "sleep 60"
+        pkill -f "sleep 300"
 else
         echo "L! Reverting"
 fi
