4e41c15b94c4275f262b713b374f7612ba71881f
mef5530 <mef5530@g.rit.edu>
2024-10-02 14:21:21 -0400

    fixing nft bugs

--- minecraft-rustdesk-safe-nftables/nftables-safe-apply.sh
+++ minecraft-rustdesk-safe-nftables/nftables-safe-apply.sh
@@ -3,13 +3,14 @@
 nft list ruleset > backup-nft.nft
 nft -f lockdown-nftables.nft
 (sleep 300 & nft -f backup-nft.nft) &
+REVERT_PID=$!
 
 echo "type confirm"
 read -t 300 confirm
 
 if [ "$confirm" ]; then
   echo "w!"
-  pkill -f "sleep 300"
+  kill $REVERT_PID
 else
   echo "l!"
 fi
